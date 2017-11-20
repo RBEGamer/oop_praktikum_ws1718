371ed6046503da95da9bde0491c3ceb61239383e
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2017-11-20 19:44:30 +0100

    added prak 2 b

---
+++ src/OOP_WS1718_PRAK_2/src/de/marcelochsendorf/BenutzerVerwaltung.java
@@ -0,0 +1,22 @@
+/**
+ interface BenutzerVerwaltung
+ @version 1.0
+ creation_date 20.11.2017
+ source_url https://github.com/RBEGamer/oop_praktikum_ws1718
+ desc Interface für die Benutzerverwaltung
+ */
+package de.marcelochsendorf;
+public interface BenutzerVerwaltung {
+		/** trägt einen neuen benutzer ein */
+		/** @param _benutzer punkt zum checken*/
+		/** @return void */
+		 void benutzereintragen(Benutzer _benutzer);
+		/** schaut ob der benutzer valide ist */
+		/** @param _benutzer benutzer zum checken*/
+		/** @return boolean if valid the true */
+		 boolean benutzerOk(Benutzer _benutzer);
+
+		/** listet alle gespeicherten benutzer auf */
+		/** @return void */
+		void listBenutzer();
+}
